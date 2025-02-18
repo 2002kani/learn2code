b9ab00df54d1f07b1a45edcc1f66efc7a4964062
02Kani <kanikaradag02@gmail.com>
2025-02-19 00:27:02 +0100

    day 1 finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { use } from "react";
 
 function App() {
 
@@ -19,14 +20,58 @@ function App() {
 
   const signUp = (name: string, alter: number, isPaid: boolean) => {}
   
-  signUp("kani", 23, true); */
+  signUp("kani", 23, true);
 
+  const Chars = ["Zorro", "Nami", "Luffy", "Usopp"];
 
+  Chars.map((char): void => {
+    console.log(`Hallo, mein name ist ${char}`);
+  })
 
+  const consoleLog = (text: string):void => {
+    console.log(text);
+  }
 
-
-
+  consoleLog("Hallo ich mache hier mal einen beispiel text");
   
+  interface User {
+    name: string,
+    alter: number,
+    isStudent: boolean,
+    is2m?: boolean,
+  }
+
+  const userInfo = (u: User):void => {
+    console.log("Name: " + u.name );
+    console.log("Alter: " + u.alter);
+    console.log("Student? " + u.isStudent);
+  }
+
+  userInfo({name: "Kani", alter: 22, isStudent: true});
+
+  interface User {
+    name: string,
+    age: number,
+    birth: number,
+    area: string
+  }
+  
+  interface User {
+    random: string
+  }
+
+// User besitzt nun auch random 
+
+  // Array erstellen, der in einer Function übergeben wird und dort gemapped wird
+  // sodass jeder einzelne charakter im Array sich vorstellt.
+  const CharArray = ["Luffy", "Zorro", "Nami"];
+
+  const numberFunction = (arr: Array<string>) => {
+    arr.map((char): void => {
+      console.log(`Mein name ist ${char}`);
+    })
+  }
+  numberFunction(CharArray); */
   
   return (
     <></>
