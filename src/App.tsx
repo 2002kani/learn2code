facc00e7e280b033847f6432ed19435d6b7ad49c
02Kani <kanikaradag02@gmail.com>
2025-02-19 00:03:23 +0100

    #

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
-import './App.css'
 
 function App() {
 
+  /*let userId: string;
+  
+  const testFunc = () => {
+    return "HALLOOOO..";
+  }
+
+  userId = testFunc();
+  
+  console.log(userId);
+
+  const addTwo = (value: number):number => {
+    return value + 2;
+  }
+
+  console.log(addTwo(1));
+
+  const signUp = (name: string, alter: number, isPaid: boolean) => {}
+  
+  signUp("kani", 23, true); */
+
+
+
+
+
+
+  
+  
   return (
     <></>
   )
