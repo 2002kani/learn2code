4d2e8333f6290a2cc256ff592a36126894961a9b
02Kani <kanikaradag02@gmail.com>
2025-03-05 14:18:17 +0100

    #

---
+++ src/Fitness/Popup/AddWorkout.tsx
@@ -0,0 +1,11 @@
+import "./AddWorkout.css"
+
+const AddWorkout = () => {
+    return(
+        <div className="popup">
+            <input type="text" />
+        </div>
+    );
+}
+
+export default AddWorkout
--- src/Fitness/Workouts.tsx
+++ src/Fitness/Workouts.tsx
@@ -1,14 +1,64 @@
+import { useState } from "react";
 import "./Workouts.css"
 import { useGetWorkouts, useAddWorkout, useUpdtateWorkout, useDeleteWorkout } from "./WorkoutsAPI";
 
 interface Workout {
-    
+    id: number;
+    exercise: string;
+    reps: number;
+    sets: number;
+    notes: string;
 }
 
 const Workouts = () => {
+
+    const addWorkout = useAddWorkout();
+    const updateWorkout = useUpdtateWorkout();
+    const deleteWorkout = useDeleteWorkout();
+
+    const { isLoading, data, error } = useGetWorkouts();
+
+    const [newWorkout, setNewWorkout] = useState("");
+
+    if(isLoading) return <h1> Lädt.. </h1>
+    if(error) return <h1> Fehler beim Laden der Seite </h1>
+
+    const handleAddWorkout = () => {
+        
+    }
+
+
     return(
         <div className="workouts">
-            <h1>hi</h1>
+            <h1> Fitness App </h1>
+
+            <div className="workout-list">
+                <div className="header">
+                    <h2> Workout Liste </h2>
+                    <button> + Add </button>
+                </div>
+
+                <ul>
+                    {data.map((workout: Workout) => (
+                        <div className="workout" key={workout.id}>
+                            <div className="top">
+                                <h3> {workout.exercise} </h3>
+                                <button> x </button>
+                            </div>
+
+                            <div className="numbers">
+                                <li> Wiederholungen: {workout.reps} </li>
+                                <li> Sätze: {workout.sets} </li>
+                            </div>
+
+                            <div className="bottom">
+                                <p> {workout.notes} </p>
+                                <button> Update </button>
+                            </div>
+                        </div>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
