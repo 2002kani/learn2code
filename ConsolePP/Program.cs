e65dcc4fba2400023ff9978e34e5015ff7d201f3
02Kani <kanikaradag02@gmail.com>
2025-09-20 20:42:08 +0200

    learn c#

---
+++ ConsolePP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{ 
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            int age = 200;
+
+            var newAge = age.ToString();
+
+            string newestAge = newAge;  
+
+        }
+
+   
+    }
+}
