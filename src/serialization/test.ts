c86297eed121e1601c2d2f343e4a607e124a09ed
02Kani <kanikaradag02@gmail.com>
2025-09-30 17:10:09 +0200

    #

--- src/serialization/test.ts
+++ src/serialization/test.ts
@@ -1,5 +1,15 @@
-const text = "hallo text!";
+interface IPerson {
+  id: number;
+  name: string;
+  age: number;
+  birth: string;
+}
 
-JSON.stringify(text);
+const person: IPerson = {
+  id: 1,
+  name: "Kani",
+  age: 23,
+  birth: "Unna",
+};
 
-console.log(text);
+console.log(person);
