412bff1945af963e993b2841ed6cb3cb1953e9aa
02Kani <kanikaradag02@gmail.com>
2025-10-05 22:50:05 +0200

    #

--- src/serialization/test.ts
+++ src/serialization/test.ts
@@ -13,10 +13,17 @@ const person: IPerson = {
 };
 
 const person2: IPerson = {
-  id: 1,
-  name: "Kani",
-  age: 23,
-  birth: "Unna",
+  id: 2,
+  name: "Test Person",
+  age: 21,
+  birth: "Bayern",
 };
 
-console.log(person, person2);
+const person3: IPerson = {
+  id: 3,
+  name: "Tim",
+  age: 34,
+  birth: "Polen",
+};
+
+console.log(person, person2, person3);
